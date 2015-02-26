5d6f06de2bbf3e30319d999f14a493368c0274a4
Reinhard Pötz <reinhard.poetz@indoqa.com>
2015-02-26 18:50:08 +0100

    use logging to write received events

--- logspace-hq-webapp/src/main/java/io/logspace/hq/webapp/resource/EventResource.java
+++ logspace-hq-webapp/src/main/java/io/logspace/hq/webapp/resource/EventResource.java
@@ -3,13 +3,28 @@ package io.logspace.hq.webapp.resource;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
 import com.indoqa.spark.AbstractResourcesBase;
 
 @Named
 public class EventResource extends AbstractResourcesBase {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @PostConstruct
     public void mount() {
-        this.postAsJson("/events", (req, res) -> "foo");
+        Spark.post("/events", (req, res) -> this.logEvents(req, res));
+    }
+
+    private String logEvents(Request req, Response res) {
+        this.logger.info(req.body());
+        res.status(202);
+        return "";
     }
 }
