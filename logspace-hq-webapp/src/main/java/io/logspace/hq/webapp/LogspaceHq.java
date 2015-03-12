87af4d92d7859554b73793a1f6b4ddddcf7fc9c6
Reinhard Pötz <reinhard.poetz@indoqa.com>
2015-03-12 20:21:46 +0100

    add EPLv1

--- logspace-hq-webapp/src/main/java/io/logspace/hq/webapp/LogspaceHq.java
+++ logspace-hq-webapp/src/main/java/io/logspace/hq/webapp/LogspaceHq.java
@@ -1,3 +1,10 @@
+/*
+ * Logspace
+ * Copyright (c) 2015 Indoqa Software Design und Beratung GmbH. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License Version 1.0, which accompanies this distribution and
+ * is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package io.logspace.hq.webapp;
 
 import java.io.IOException;
@@ -20,10 +27,6 @@ public class LogspaceHq extends AbstractSparkApplication {
     @Override
     protected String getApplicationName() {
         return "Logspace";
-    }
-
-    protected String getAsciiLogo() throws IOException {
-        return IOUtils.toString(LogspaceHq.class.getResourceAsStream("/logspace.io.txt"));
     }
 
     @Override
