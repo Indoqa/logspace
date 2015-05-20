96c34b92bbbea7b31490371fa387c9690f626d66
Reinhard Pötz <reinhard.poetz@indoqa.com>
2015-05-20 17:59:07 +0200

    debounce function: to ES6

--- logspace-frontend/src/lib/debounce.js
+++ logspace-frontend/src/lib/debounce.js
@@ -12,15 +12,15 @@ export default function debounceFunc(func, wait, immediate) {
 	return () => {
 		var context = this, args = arguments;
 
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
+		var later = () => {
+			timeout = null
+			if (!immediate) func.apply(context, args)
+		}
 
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
+		var callNow = immediate && !timeout
+		clearTimeout(timeout)
 
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
+		timeout = setTimeout(later, wait)
+		if (callNow) func.apply(context, args)
 	}
 }
