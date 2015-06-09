81662048cb6fdb9523dc34e05b1d3363a0cc97b6
Reinhard Pötz <reinhard.poetz@indoqa.com>
2015-06-09 23:46:14 +0200

    license header

--- logspace-frontend/src/lib/state.js
+++ logspace-frontend/src/lib/state.js
@@ -1,3 +1,10 @@
+/*
+ * Logspace
+ * Copyright (c) 2015 Indoqa Software Design und Beratung GmbH. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License Version 1.0, which accompanies this distribution and
+ * is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 import EventEmitter from 'eventemitter3';
 import immutable from 'immutable';
 
