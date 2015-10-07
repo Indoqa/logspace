77f3bab41c3323359e173a3363d4020b26285905
Reinhard Pötz <reinhard.poetz@indoqa.com>
2015-10-07 19:23:28 +0200

    move solr conf to META-INF/solr; move solr related properties to webapp (in order to make the logspace-hq-solr-plugin usable within a different environment

--- logspace-hq-solr-plugin/src/main/java/io/logspace/hq/solr/SolrPluginConfiguration.java
+++ logspace-hq-solr-plugin/src/main/java/io/logspace/hq/solr/SolrPluginConfiguration.java
@@ -14,12 +14,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import com.indoqa.solr.spring.client.SolrClientFactory;
 
 @Configuration
-@PropertySource("classpath:/io/logspace/hq/solr/logspace-hq-solr.properties")
 public class SolrPluginConfiguration {
 
     @Value("${logspace.solr.base-url}")
