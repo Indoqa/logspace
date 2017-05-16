1af11d6de4eba38e41d5214730beb1e27f986a09
Steven Dolg <steven.dolg@indoqa.com>
2017-05-16 12:00:25 +0200

    - removed obsolete file - added missing license headers

--- logspace-frontend-new/src/main/app/components/autoplay/RunIcon.react.js
+++ logspace-frontend-new/src/main/app/components/autoplay/RunIcon.react.js
@@ -1,4 +1,11 @@
-import React, {PropTypes} from 'react'
+/*
+ * Logspace
+ * Copyright (c) 2015 Indoqa Software Design und Beratung GmbH. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License Version 1.0, which accompanies this distribution and
+ * is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+ import React, {PropTypes} from 'react'
 import {FontAwesome} from 'indoqa-rebass-components'
 import 'font-awesome/css/font-awesome.css'
 
